14fd9c23e32e519210bdcecd2694317b22363ff3
Asad-Ali-Rajput <asadalirajput453@gmail.com>
2023-12-05 20:05:11 +0500

    Axios and Toastification is Implemented

--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -3,7 +3,8 @@ import { EyeIcon, EyeSlashIcon } from '@heroicons/vue/24/outline'
 import { ref } from 'vue'
 import { useForm, createSubmitHandler } from 'vue-use-form'
 import { useRouter } from 'vue-router';
-// import { useToast } from 'vue-toastification'
+import { useToast } from 'vue-toastification'
+import axios from 'axios'
 
 const toast = useToast()
 const router = useRouter()
@@ -35,7 +36,7 @@ const onSubmit = createSubmitHandler(async (data) => {
       // Redirect or perform other actions upon successful login
       router.push('/profile')
     } catch (error) {
-      toast.error(String(err))
+      toast.error(String(error))
     }
   })
 </script>
