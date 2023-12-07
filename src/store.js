5cfe971cb14b179260bcac949dfbdbdc645b1093
Asad-Ali-Rajput <asadalirajput453@gmail.com>
2023-12-07 23:33:55 +0500

    removed used and comented code

--- src/store.js
+++ src/store.js
@@ -1,105 +1,59 @@
-// import { createStore } from 'vuex'
-// import createPersistedState from 'vuex-persistedstate'
-
-// export default createStore({
-//   state: {
-//     accessToken: null,
-//     refreshToken: null,
-//     isOpenModal: false,
-//   },
-//   mutations: {
-//     setTokens(state, { accessToken, refreshToken }) {
-//       state.accessToken = accessToken
-//       state.refreshToken = refreshToken
-//     },
-//     clearTokens(state) {
-//       state.accessToken = null
-//       state.refreshToken = null
-//     },
-//     openModal(state) {
-//       state.isOpenModal = true
-//     },
-//     closeModal(state) {
-//       state.isOpenModal = false
-//     },
-//   },
-//   actions: {
-//     login({ commit }, { accessToken, refreshToken }) {
-//       commit('setTokens', { accessToken, refreshToken })
-//     },
-//     logout({ commit }) {
-//       commit('clearTokens')
-//     },
-//     openModal({ commit }) {
-//       commit('openModal')
-//     },
-//     closeModal({ commit }) {
-//       commit('closeModal')
-//     },
-//   },
-//   getters: {
-//     getAccessToken: state => state.accessToken,
-//     getRefreshToken: state => state.refreshToken,
-//   },
-//   plugins: [createPersistedState()],
-// })
-
-import { createStore } from 'vuex';
-import createPersistedState from 'vuex-persistedstate';
+import { createStore } from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
 
 export default createStore({
   state: {
     accessToken: null,
     refreshToken: null,
     isOpenModal: false,
-    otp: null, // New OTP state
+    otp: null,
   },
   mutations: {
     setTokens(state, { accessToken, refreshToken }) {
-      state.accessToken = accessToken;
-      state.refreshToken = refreshToken;
+      state.accessToken = accessToken
+      state.refreshToken = refreshToken
     },
     clearTokens(state) {
-      state.accessToken = null;
-      state.refreshToken = null;
+      state.accessToken = null
+      state.refreshToken = null
     },
     openModal(state) {
-      state.isOpenModal = true;
+      state.isOpenModal = true
     },
     closeModal(state) {
-      state.isOpenModal = false;
+      state.isOpenModal = false
     },
-    setOtp(state, otp) { // New mutation to set OTP
-      state.otp = otp;
+    setOtp(state, otp) {
+      state.otp = otp
     },
-    clearOtp(state) { // New mutation to clear OTP
-      state.otp = null;
+    clearOtp(state) {
+      state.otp = null
     },
   },
   actions: {
     login({ commit }, { accessToken, refreshToken }) {
-      commit('setTokens', { accessToken, refreshToken });
+      commit('setTokens', { accessToken, refreshToken })
     },
     logout({ commit }) {
-      commit('clearTokens');
+      commit('clearTokens')
     },
     openModal({ commit }) {
-      commit('openModal');
+      commit('openModal')
     },
     closeModal({ commit }) {
-      commit('closeModal');
+      commit('closeModal')
     },
-    setOtp({ commit }, otp) { // New action to set OTP
-      commit('setOtp', otp);
+    setOtp({ commit }, otp) {
+      commit('setOtp', otp)
     },
-    clearOtp({ commit }) { // New action to clear OTP
-      commit('clearOtp');
+    clearOtp({ commit }) {
+      commit('clearOtp')
     },
   },
   getters: {
     getAccessToken: state => state.accessToken,
     getRefreshToken: state => state.refreshToken,
-    getOtp: state => state.otp, // New getter for OTP
+    getOtp: state => state.otp, 
   },
   plugins: [createPersistedState()],
   modules: {
@@ -113,24 +67,24 @@ export default createStore({
       },
       mutations: {
         setUser(state, { name, email, role, password }) {
-          state.name = name;
-          state.email = email;
-          state.role = role;
-          state.password = password;
+          state.name = name
+          state.email = email
+          state.role = role
+          state.password = password
         },
         clearUser(state) {
-          state.name = null;
-          state.email = null;
-          state.role = null;
-          state.password = null;
+          state.name = null
+          state.email = null
+          state.role = null
+          state.password = null
         },
       },
       actions: {
         setUser({ commit }, { name, email, role, password }) {
-          commit('setUser', { name, email, role, password });
+          commit('setUser', { name, email, role, password })
         },
         clearUser({ commit }) {
-          commit('clearUser');
+          commit('clearUser')
         },
       },
       getters: {
@@ -138,4 +92,4 @@ export default createStore({
       },
     },
   },
-});
+})
