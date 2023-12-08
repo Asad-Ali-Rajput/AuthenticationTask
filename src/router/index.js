1b6771f0547e8f94daf8a25c209f397c2c2b118d
Asad-Ali-Rajput <asadalirajput453@gmail.com>
2023-12-09 04:37:01 +0500

    New route for edit product created

--- src/router/index.js
+++ src/router/index.js
@@ -18,12 +18,20 @@ const router = createRouter({
       component: () => import('../views/SignUpView.vue')
     },
     {
-      path: '/profile',
-      name: 'profile',
+      path: '/home',
+      name: 'home',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/ProfileView.vue')
+      component: () => import('../views/ProductView.vue')
+    },
+    {
+      path: '/editProduct',
+      name: 'editProduct',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/ProductDetails.vue')
     }
   ]
 })
