756668653f372ac516e34c772aeb6055a0b815cd
Asad-Ali-Rajput <asadalirajput453@gmail.com>
2023-12-08 22:29:38 +0500

    display only 4 inputs for OTP

--- src/views/OTPView.vue
+++ src/views/OTPView.vue
@@ -13,7 +13,7 @@ export default {
             <div class="absolute inset-0 bg-gray-900 opacity-70"></div>
             <div id="popup-modal" tabindex="-1"
                 class="relative p-4 w-2/3 flex justify-center items-center max-w-2/3 max-h-full h-2/3 rounded-lg bg-white">
-                <Otp :digit-count="6"></Otp>
+                <Otp :digit-count="4"></Otp>
             </div>
         </div>
     </div>
