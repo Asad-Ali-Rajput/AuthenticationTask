e64f3f9e439c31a06e9b8e08d46534657598dd10
Asad-Ali-Rajput <asadalirajput453@gmail.com>
2023-12-08 22:28:14 +0500

    Validation Implemented bugs removed

--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -32,13 +32,13 @@ export default {
   methods: {
     togglePasswordVisibility() {
       this.showPassword = !this.showPassword;
-      this.passwordInputType = this.showPassword ? 'text' : 'password';
+      this.passwordInputType = this.showPassword ? 'text' : 'password'
     },
     async onSubmit(data) {
       try {
         const response = await axios.post('http://localhost:5000/api/user/login', {
-          email: data.email,
-          password: data.password,
+          email: data.email.el._value,
+          password: data.password.el._value,
         });
         const { token, refreshToken } = response.data;
         // Store the tokens in store's state for later use
@@ -47,11 +47,14 @@ export default {
           refreshToken: refreshToken,
         });
         // Redirect or perform other actions upon successful login
-        this.route.push('/profile');
+        this.route.push('/profile')
       } catch (error) {
         this.toast.error(String(error))
       }
     },
+    onError(errors) {
+      console.log(errors)
+    }
   },
 }
 </script>
@@ -59,30 +62,34 @@ export default {
 <template>
   <div class="w-full h-full flex">
     <div class="w-1/2 flex justify-center items-center">
-      <form class="w-[70%]" @submit.prevent="() => handleSubmit(onSubmit)()">
+      <form class="w-[70%]" @submit.prevent="() => handleSubmit(onSubmit, onError)()">
         <h1 class="text-2xl font-semibold font-sans">Sign In</h1>
         <p class="text-gray-400">Enter your email and password to sign in</p>
         <div>
           <div class="py-6 relative">
-            <input type="email" id="email" name="email"
-              class="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-violet-500 focus:border-violet-500 block w-full p-2.5 mb-4 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-violet-500 dark:focus:border-violet-500"
-              placeholder="Email" required :="register('email', { required: true })">
-            <span v-if="errors.email">{{ errors.email.message }}</span>
-            <div class="relative">
-              <input id="password" name="password"
-                class="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-violet-500 focus:border-violet-500 block w-full p-2.5 mb-4 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-violet-500 dark:focus:border-violet-500"
-                :type="passwordInputType" placeholder="Password" required
-                :="register('password', { required: 'Password field cannot be empty!' })">
-              <button type="button" class="absolute bottom-2 right-3" @click="togglePasswordVisibility()">
-                <span v-if="!showPassword">
-                  <EyeSlashIcon class="h-6 w-6 text-violet-500" />
-                </span>
-                <span v-else>
-                  <EyeIcon class="h-6 w-6 text-violet-500" />
-                </span>
-              </button>
+            <div class="pb-4">
+              <input type="email" id="email" name="email"
+                class="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-violet-500 focus:border-violet-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-violet-500 dark:focus:border-violet-500"
+                placeholder="Email" v-bind="register('email', { required: 'Email field cannot be empty!' })">
+              <span class="text-red-500" v-if="errors.email">{{ errors.email.message }}</span>
             </div>
-            <span v-if="this.errors.password">{{ this.errors.password }}</span>
+            <div class=" pb-4">
+              <div class="relative">
+                <input id="password" name="password"
+                  class="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-violet-500 focus:border-violet-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-violet-500 dark:focus:border-violet-500"
+                  :type="passwordInputType" placeholder="Password"
+                  v-bind="register('password', { required: 'Password field cannot be empty!' })">
+                <button type="button" class="absolute bottom-2 right-3" @click="togglePasswordVisibility">
+                  <span v-if="!showPassword">
+                    <EyeSlashIcon class="h-6 w-6 text-violet-500" />
+                  </span>
+                  <span v-else>
+                    <EyeIcon class="h-6 w-6 text-violet-500" />
+                  </span>
+                </button>
+              </div>
+              <span class="text-red-500" v-if="errors.password">{{ errors.password.message }}</span>
+            </div>
             <RouterLink to="#" class="text-violet-500 active:text-violet-500">Forget Password?</RouterLink>
           </div>
         </div>
