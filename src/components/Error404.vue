20e487dea5c912c7291e82bfa989ee20ce27c1d0
Asad-Ali-Rajput <asadalirajput453@gmail.com>
2023-12-14 01:42:27 +0500

    new component for error 404 created

---
+++ src/components/Error404.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="flex justify-center items-center w-screen h-screen">
+        <img src="../assets/Error.webp" alt="Error Image" />
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+    
+</style>
