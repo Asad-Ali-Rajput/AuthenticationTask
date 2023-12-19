fb931278598a1549ff7e9f4cdc7e6f320a7d17f6
Asad-Ali-Rajput <asadalirajput453@gmail.com>
2023-12-20 04:38:30 +0500

    Layout created

---
+++ src/layout/Layout.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+      <!-- Navbar component can be placed here -->
+      <Navbar />
+      
+      <!-- Main content area -->
+      <div class="main-content">
+        <router-view></router-view>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import Navbar from '../components/Navbar.vue'; // Replace with the actual path to your Navbar component
+  
+  export default {
+    components: {
+      Navbar,
+    },
+  };
+  </script>
+  
+  <style>
+  /* Add styling for your layout component */
+  </style>
+  
