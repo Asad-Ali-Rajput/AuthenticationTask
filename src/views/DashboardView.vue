b8e79b561500b1c8221e02a5fc19ebb08c6adc5b
Asad-Ali-Rajput <asadalirajput453@gmail.com>
2024-01-03 03:21:27 +0500

    Dashboard view created

---
+++ src/views/DashboardView.vue
@@ -0,0 +1,16 @@
+<template lang="">
+    <main>
+        <Dashboard />
+    </main>
+</template>
+<script>
+import Dashboard from '../components/Dashboard.vue'
+export default {
+    components: {
+        Dashboard
+    }
+}
+</script>
+<style lang="">
+    
+</style>
