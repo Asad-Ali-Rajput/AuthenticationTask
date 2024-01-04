559fbf91085a862c096cba2eabeec933e1a4d958
Asad-Ali-Rajput <asadalirajput453@gmail.com>
2024-01-05 04:07:21 +0500

    Sub Sections in section 1 created

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -3,7 +3,12 @@
         <div>Navbar</div>
         <div>Sub Navbar</div>
         <div>
-            <div>section 1</div>
+            <div>section 1
+                <div>Sub Section1</div>
+                <div>Sub Section2</div>
+                <div>Sub Section3</div>
+                <div>Sub Section4</div>
+            </div>
             <div>section 2</div>
             <div>section 3</div>
         </>
