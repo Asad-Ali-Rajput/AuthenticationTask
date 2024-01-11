2b73809e3302c42c1eaf7530ed5f37807d50c3a6
Asad-Ali-Rajput <asadalirajput453@gmail.com>
2024-01-12 04:20:34 +0500

    New route created for edi Inspactor

--- src/router/index.js
+++ src/router/index.js
@@ -45,6 +45,12 @@ const router = createRouter({
           component: () => import('../views/AddProductView.vue'),
           props: true,
         },
+        {
+          path: 'edinspactor',
+          name: 'edInspactor',
+          component: () => import('../views/EDInspactorView.vue'),
+          props: true,
+        },
       ]
     },
     // {
