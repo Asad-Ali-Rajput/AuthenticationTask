a5a4a21c8c9f39e87ac834409a5453f72f68c5d7
Asad-Ali-Rajput <asadalirajput453@gmail.com>
2024-01-12 04:22:59 +0500

    EDI Inpector View created

--- src/views/EDInspactorView.vue
+++ src/views/EDInspactorView.vue
@@ -1,11 +1,14 @@
 <template lang="">
     <div>
-        
+        <EdiInspactor />
     </div>
 </template>
 <script>
+import EdiInspactor from '../components/EdiInspactor.vue';
 export default {
-    
+    components: {
+        EdiInspactor,
+    }
 }
 </script>
 <style lang="">
